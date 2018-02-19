11605eb8756395c8054362ac5148aee3f370ffcf
Rubin_Li <110010001r@gmail.com>
2018-02-19 20:43:00 +0000

    Stack test

---
+++ task_1/src/com/Ivlev/Stack/SimpleStack.java
@@ -0,0 +1,8 @@
+package com.Ivlev.Stack;
+
+public interface SimpleStack{
+     void Push(Object object) ;
+    //oid Pop();
+    int Peek();
+    //boolean Empty();
+}
---
+++ task_1/src/com/Ivlev/Stack/Tests/TestStack.java
@@ -0,0 +1,19 @@
+package com.Ivlev.Stack.Tests;
+
+import com.Ivlev.Stack.Stack;
+import com.Ivlev.Stack.SimpleStack;
+
+
+public class TestStack {
+        public static void main(String[] args) {
+                Object object = new Object();
+                Stack TestStack0 = new Stack(60);
+               TestStack0.Push(object);
+
+
+
+              Stack TestStack1 = new Stack(-50);
+              Stack TestStack2 = new Stack(5000);
+        }
+}
+
